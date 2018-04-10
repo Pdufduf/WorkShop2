88f5aef64c543a8fb57c424e3245a6b8c481da7f
Pdufduf <31993533+Pdufduf@users.noreply.github.com>
2018-04-10 14:47:48 +0200

    BDD

--- Script/verifConnexion.php
+++ Script/verifConnexion.php
@@ -22,14 +22,28 @@ if(isset($_POST['connexion'])) { // si le bouton "Connexion" est appuyé
             // les champs sont bien postés et pas vide, on sécurise les données entrées par l'étudiant:
             $email = htmlentities($_POST['email'], ENT_QUOTES, "ISO-8859-1"); // le htmlentities() passera les guillemets en entités HTML, ce qui empêchera les injections SQL
             $MotDePasse = htmlentities($_POST['mdp'], ENT_QUOTES, "ISO-8859-1");
+
             //on se connecte à la base de données:
-            $mysqli = mysqli_connect("localhost", "root", "root", "helpcenter");
+            try {
+                $dbName = 'helpcenter';
+                $host = 'localhost';
+                $utilisateur = 'root';
+                $motDePasse = 'root';
+                $port = '3307';
+                $dns = 'mysql:host=' . $host . ';dbname=' . $dbName . ';port=' . $port;
+                $connection = new PDO($dns, $utilisateur, $motDePasse);
+            }
+            catch ( Exception $e){
+                    echo "Connexion à la BDD impossible : ", $e->getMessage();
+                    die();
+            }
+
             //on vérifie que la connexion s'effectue correctement:
-            if(!$mysqli){
+            if(!$pdo){
                 echo "Erreur de connexion à la base de données.";
             } else {
                 // on fait maintenant la requête dans la base de données pour rechercher si ces données existe et correspondent:
-                $Requete = mysqli_query($mysqli,"SELECT * FROM etudiant WHERE email = '".$email."' AND mdp = '".md5($MotDePasse)."'");//si vous avez enregistré le mot de passe en md5() il vous suffira de faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
+                $Requete = mysqli_query($pdo,"SELECT * FROM etudiant WHERE email = '".$email."' AND mdp = '".md5($MotDePasse)."'");//si vous avez enregistré le mot de passe en md5() il vous suffira de faire la vérification en mettant mdp = '".md5($MotDePasse)."' au lieu de mdp = '".$MotDePasse."'
                 // si il y a un résultat, mysqli_num_rows() nous donnera alors 1
                 // si mysqli_num_rows() retourne 0 c'est qu'il a trouvé aucun résultat
                 if(mysqli_num_rows($Requete) == 0) {
