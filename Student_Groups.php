ee7d6d2ea6ddb6ebdc62eec579ff36518c3260c0
E-Cantor <38208060+E-Cantor@users.noreply.github.com>
2018-04-11 15:09:44 +0200

    Changes #4

---
+++ Student_Groups.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Groups</title>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+<link rel="stylesheet" type="text/css" href="css/Tables.css">
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+    <div id="mySidenav" class="sidenav">
+        <h1>MENU</h1>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <p><a href="Student.html">Accueil</a></p>
+        <p><a href="Student_Question_Form.html">Formulaires de Questions</a></p>
+        <p><a href="Student_Groups.html">Groupes</a></p>
+
+        <footer>
+          <p><a href="#">Déconnexion</a></p>
+        </footer>
+    </div>
+<div class="main">
+<div style="overflow-x:auto;">
+<table style="width:100%">
+	<tr>
+		<th> Groupe </th>
+		<th> Remplissage </th>
+		<th> Join </th>
+	</tr>
+	<tr>
+		<td>Group 1</td>
+		<td>0/4</td>
+		<td> Button Place Holder </td>
+	</tr>
+	<tr>
+		<td>Group 2</td>
+		<td>0/4</td>
+		<td> Button Place Holder </td>
+	</tr>
+	<tr>
+		<td>Group 3</td>
+		<td>0/4</td>
+		<td> Button Place Holder </td>
+	</tr>
+	<tr>
+		<td>Group 4</td>
+		<td>0/4</td>
+		<td> Button Place Holder </td>
+	</tr>
+	<tr>
+		<td>Group 5</td>
+		<td>0/4</td>
+		<td> Button Place Holder </td>
+	</tr>
+		
+</table>
+</div>
+</div>
+</body>
+
+</html>
