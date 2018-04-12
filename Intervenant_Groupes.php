f44cfb5164be50ab707db449b83f67bdadd9ac96
Quentin Engelmann <quengel1@hotmail.fr>
2018-04-12 12:20:56 +0200

    Mail
    Envoie email

---
+++ Intervenant_Groupes.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Intervenant Reception de Question</title>
+<link rel="#" href="#">
+<link BDD ###>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+<link href="css/calendar.css" type="text/css" rel="stylesheet" />
+<?php
+  include 'pdo.php';
+?>
+</head>
+<body>
+    <div id="mySidenav" class="sidenav">
+        <h1>MENU</h1>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <p><a href="Intervenant.php">Accueil</a></p>
+        <p><a href="Intervenant_Reception_Question.php">Demandes</a></p>
+        <p><a href="Intervenant_Groupes.php">Groupes</a></p>
+
+        <footer>
+          <p><a href="../WorkShop2/index.php">Déconnexion</a></p>
+        </footer>
+    </div>
+	<div id="Main" class="main"?
+</body>
+</html>
