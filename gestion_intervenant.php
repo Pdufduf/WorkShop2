158eb3ef46e95b371a3251e3f81854d39bf7462a
vincentje <38208343+vincentje@users.noreply.github.com>
2018-04-12 12:30:06 +0200

    responsable etudiant

---
+++ gestion_intervenant.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gestion intervenant</title>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+<div class="sidenav">
+	<p><a href="referent.html">Acceuil</a></p>
+	<p><a href="projet.html">projet</a></p>
+</div>
+<div class="main">
+<B><U>CREATION D'UN INTERVENANT</U></B>
+<br><br><br><br>
+<form>
+	Nom du profs : <br>
+	<input type="text">
+	<br><br>
+	Prenom du profs : <br>
+	<input type="text">
+	<br><br>
+	matiere enseigné : <br>
+	<input type="text">
+  <br><br>	
+  adresse e-mail  : <br>
+	<input type="text">
+	<br><br>
+	mots de passe de connexion : <br>
+	<input type="text">
+	<br><br>
+  <input type="submit">
+</form>
+</div>
+</body>
+
+</html>
