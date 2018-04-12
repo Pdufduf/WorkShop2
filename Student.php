7133ffe085639d74e38d471997eaa150114382f5
Quentin Engelmann <quengel1@hotmail.fr>
2018-04-12 09:20:38 +0200

    Changement des pages en .php
    Changement des pages en .php

---
+++ Student.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student</title>
+<link rel="#" href="#">
+<link BDD ###>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+    <div id="mySidenav" class="sidenav">
+        <h1>MENU</h1>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <p><a href="Student.html">Accueil</a></p>
+        <p><a href="Student_Question_Form.html">Formulaires de Questions</a></p>
+        <p><a href="Student_Groups.html">Groupes</a></p>
+
+        <footer>
+          <p><a href="../WorkShop2/index.php">Déconnexion</a></p>
+        </footer>
+    </div>
+
+<!-- Use any element to open the sidenav -->
+<span onclick="openNav()">open</span>
+
+<!-- Add all page content inside this div if you want the side nav to push page content to the right (not used if you only want the sidenav to sit on top of the page -->
+<div id="main">
+Filler
+</div>
+
+</body>
+</html>
---
+++ Student_Question_Form.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Student Question Form</title>
+<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="css/Side_Bar_Navigation.css">
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
+    <div id="mySidenav" class="sidenav">
+        <h1>MENU</h1>
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <p><a href="Student.html">Accueil</a></p>
+        <p><a href="Student_Question_Form.html">Formulaires de Questions</a></p>
+        <p><a href="Student_Groups.html">Groupes</a></p>
+
+        <footer>
+          <p><a href="../WorkShop2/index.php">Déconnexion</a></p>
+        </footer>
+    </div>
+
+<div class="main">
+<form>
+	Nom du Groupe : <br>
+	<input type="text">
+	<br><br>
+	Prof : <br>
+	<select name="cars">
+		<option value="dummy@email.com">Dummy1</option>
+		<option value="dummy2@email.com">Dummy2</option>
+		<option value="dummy3@email.com">Dummy3</option>
+		<option value="dummy4@email.com">Dummy4</option>
+	</select>
+	<br><br>
+	Question : <br>
+		<textarea name="message" rows="10" cols="30"></textarea>
+  <br><br>
+  <input type="submit">
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
